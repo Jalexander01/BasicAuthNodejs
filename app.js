6c355773b8f6f8dae8e42df0c659b4b6389c2d3f
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-06-03 09:50:04 -0500

    updating

--- app.js
+++ app.js
@@ -10,19 +10,17 @@ app.use(express.static("public"));//use static files like style.css and js
 
 //get function will send information to the brower at loading
 app.get("/", function (req, res) {
-    // console.log(req);
-    // res.sendFile();
-    // res.sendFile(__dirname + "/index.html");
-    // res.send(__dirname);
-
-    //ejs
-    res.render('index');
-})
+    res.render('home');
+});
 
 //post function will receive information from the browser to the server
 app.post("/", function (req, res) {
     res.send("The results of ");
     res.redirect("/");//redirects to app.get function or another function
+});
+
+app.get("/login", function (req, res) {
+    res.render('home');
 })
 //process.env.PORT || heroku code
 app.listen(process.env.PORT || 3000, function () {
